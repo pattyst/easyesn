e781b8d59026a16c291ae60ddac5b43179d2218f
pattyst <44293996+pattyst@users.noreply.github.com>
2018-11-30 21:11:59 +0000

    update numpyBackend.py
    typo in allclose

--- src/easyesn/easyesn/backend/numpyBackend.py
+++ src/easyesn/easyesn/backend/numpyBackend.py
@@ -94,7 +94,7 @@ def var(x):
 	return np.var(x)
 
 def allclose(x, y, atol=1e-05, rtol=0, equal_nan=False):
-    return np.allclose(x, y, atol, atol, equal_nan)
+    return np.allclose(x, y, atol, rtol, equal_nan)
 
 def ptp(x, axis=None):
     return np.ptp(x, axis)
